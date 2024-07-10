e4e6aa4dcbde258d837005535173584eeb114b39
Guilherme santos <163909949+guilhermesantos78@users.noreply.github.com>
2024-07-10 13:54:05 -0300

    Mechendo no Sitema

--- TrabalhoFluxoLoja/GerenciadorFretes.cs
+++ TrabalhoFluxoLoja/GerenciadorFretes.cs
@@ -38,6 +38,36 @@ namespace Trabalho
         new FreteEstado(27, "Maranhão", 31.00),
         };
 
+        public static void Estados()
+        {
+            Console.WriteLine("1 - Acre");
+            Console.WriteLine("2 - Alagoas");
+            Console.WriteLine("3 - Amapa");
+            Console.WriteLine("4 - Amazonas");
+            Console.WriteLine("5 - Bahia");
+            Console.WriteLine("6 - Ceará");
+            Console.WriteLine("7 - Distrito Federal");
+            Console.WriteLine("8 - Espírito Santo");
+            Console.WriteLine("9 - Goiás");
+            Console.WriteLine("10 - Mato Grosso");
+            Console.WriteLine("11 - Mato Grosso do Sul");
+            Console.WriteLine("12 - Minas Gerais");
+            Console.WriteLine("13 - Pará");
+            Console.WriteLine("14 - Paraíba");
+            Console.WriteLine("15 - paraná");
+            Console.WriteLine("16 - Pernambuco");
+            Console.WriteLine("17 - Piaui");
+            Console.WriteLine("18 - Rio de Janeiro");
+            Console.WriteLine("19 - Rio Grande do Norte");
+            Console.WriteLine("20 - Rio Grande do Sul");
+            Console.WriteLine("21 - Rondônia");
+            Console.WriteLine("22 - Roraima");
+            Console.WriteLine("23 - Santa Catarina");
+            Console.WriteLine("24 - São Paulo ");
+            Console.WriteLine("25 - Sergipe");
+            Console.WriteLine("26 - Tocantis");
+            Console.WriteLine("27 - Maranhão");
+        }
 
         public static double ConsultarFretePorId(int id)
         {
@@ -51,7 +81,6 @@ namespace Trabalho
 
             return -1;
         }
-
 
     }
 }
