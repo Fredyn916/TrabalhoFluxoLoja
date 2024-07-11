c06ca8fbdac1db7df196a03505f858ded029bb95
Fred Marques <fredyn.curso@gmail.com>
2024-07-11 13:55:18 -0300

    EscolhaDeProdutos

--- TrabalhoFluxoLoja/Gerenciador.cs
+++ TrabalhoFluxoLoja/Gerenciador.cs
@@ -49,5 +49,108 @@ namespace TrabalhoFluxoLoja
             }
             return null;
         }
+
+        // VENDA
+
+        //public void AdicionarUsuario(Usuario x) (MÉTODO PARA ALTERAR ID DA LISTA DAS COMPRAS)
+        //{
+        //    int novoID = UsuariosCadastrados.Max(x => x.Id) + 1;
+        //    x.AlterarIdUsuario(novoID);
+        //    UsuariosCadastrados.Add(x);
+        //}
+
+        public void RealizarVenda(Usuario usuario)
+        {
+            int Id = 0;
+            double ValorTotalVenda = 0.0;
+            string Estado = string.Empty;
+            DateTime DataVenda = DateTime.Now;
+        }
+
+        public List<Produto> ProdutosEscolhidosFisicos()
+        {
+            List<Produto> produtosEscolhidosFisicos = new List<Produto>();
+            int acao = -1;
+            int id = -1;
+
+            while (acao != 2)
+            {
+                acao = MenuEscolhaProdutos();
+                if (acao == 1)
+                {
+                    Quadro.ListarProdutoFisico();
+                    while(id < 1 && id > 10)
+                    {
+                        Console.WriteLine($"Digite o Id do produto que deseja selecionar");
+                        id = int.Parse(Console.ReadLine());
+                        if (id < 1 && id > 10)
+                        {
+                            Console.WriteLine("Digite um Id válido");
+                        }
+                    }
+                    Produto produtoParaAdicionar = Quadro.BuscarProdutoFisicoPorId(id);
+                    produtosEscolhidosFisicos.Add(produtoParaAdicionar);
+                }
+                else if (acao == 2)
+                {
+                    Console.WriteLine("Seleção Finalizada");
+                }
+            }
+
+            return produtosEscolhidosFisicos;
+        }
+
+        public List<Produto> ProdutosEscolhidosDigitais()
+        {
+            List<Produto> produtosEscolhidosDigitais = new List<Produto>();
+            int acao = -1;
+            int id = -1;
+
+            while (acao != 2)
+            {
+                acao = MenuEscolhaProdutos();
+                if (acao == 1)
+                {
+                    Quadro.ListarProdutoDigital();
+                    while (id < 1 && id > 10)
+                    {
+                        Console.WriteLine($"Digite o Id do produto que deseja selecionar");
+                        id = int.Parse(Console.ReadLine());
+                        if (id < 1 && id > 10)
+                        {
+                            Console.WriteLine("Digite um Id válido");
+                        }
+                    }
+                    Produto produtoParaAdicionar = Quadro.BuscarProdutoDigitalPorId(id);
+                    produtosEscolhidosDigitais.Add(produtoParaAdicionar);
+                }
+                else if (acao == 2)
+                {
+                    Console.WriteLine("Seleção Finalizada");
+                }
+            }
+
+            return produtosEscolhidosDigitais;
+        }
+
+        private int MenuEscolhaProdutos()
+        {
+            int escolha = 0;
+
+            while(escolha != 1 && escolha != 2)
+            {
+                Console.WriteLine($"<----- ESCOLHA ----->");
+                Console.WriteLine($"1 - Escolher produto");
+                Console.WriteLine($"2 - Finalizar escolha");
+                Console.WriteLine($"<------------------->");
+                escolha = int.Parse(Console.ReadLine());
+                if(escolha != 1 && escolha != 2)
+                {
+                    Console.WriteLine("Digite uma opção válida");
+                }
+            }
+
+            return escolha;
+        }
     }
 }
