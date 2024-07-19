27968137d21d99cf42e169b471fee619b363c148
Guilherme santos <163909949+guilhermesantos78@users.noreply.github.com>
2024-07-19 16:06:52 -0300

    s

--- TrabalhoFluxoLoja/Program.cs
+++ TrabalhoFluxoLoja/Program.cs
@@ -6,7 +6,4 @@ Sistema s = new Sistema();
 
 //s.InicializarSistema();
 
-GerenciadorFretes.Estados();
-Console.WriteLine("digite o estado que deseja consultar ");
-int Frete = int.Parse(Console.ReadLine());
-GerenciadorFretes.ConsultarFretePorId(Frete);
+s.ConsultarRetornarFrete();
